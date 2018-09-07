f81af7b0b3647206c0d1072b1ca940186f499a90
Tzviki Wolvovsky <tzviki@rallybound.com>
2018-09-07 08:24:34 -0400

    Added unsplashRequest

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -7,6 +7,26 @@
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
+        searchedForText = searchField.value;        
+        const unsplashRequest = new XMLHttpRequest();
+
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 5000f3552ab119ef0ceab34398bbd269a7e6d497c24f401302a928d7d406b062');
+        unsplashRequest.send();
+
+        function addImage() {
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+            const firstImage = data.results[0];
+
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+
+        }
     });
 })();
